57aa3166677488ae53d753fa5c9d3f7b8b2c235c
kathemica <kathemica@gmail.com>
2024-05-04 12:57:50 -0300

    added card service structure

---
+++ card-service/src/main/java/com/kathesama/app/master/microservices/service/card/CardserviceApplication.java
@@ -0,0 +1,12 @@
+package com.kathesama.app.master.microservices.service.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardserviceApplication.class, args);
+	}
+}
