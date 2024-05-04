f53896a5b9319ea8071dd195e970d7b36179e2f0
kathemica <kathemica@gmail.com>
2024-05-04 17:46:10 -0300

    starting: added loan service structure

---
+++ loan-service/src/main/java/com/kathesama/app/master/microservices/service/loan/LoanserviceApplication.java
@@ -0,0 +1,12 @@
+package com.kathesama.app.master.microservices.service.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanserviceApplication.class, args);
+	}
+}
---
+++ loan-service/src/test/java/com/kathesama/app/master/microservices/service/loan/LoanserviceApplicationTest.java
@@ -0,0 +1,18 @@
+package com.kathesama.app.master.microservices.service.loan;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+public class LoanserviceApplicationTest {
+
+	@BeforeAll
+	public void init() {}
+
+	@Test
+	public void contextLoads() {
+	}
+}
