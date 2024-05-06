5514acfe9c53c45e493621b0ccad325aa08f1936
kathemica <kathemica@gmail.com>
2024-05-05 23:39:36 -0300

    adding changes to property files

--- card-service/src/main/java/com/kathesama/app/master/microservices/service/card/CardserviceApplication.java
+++ card-service/src/main/java/com/kathesama/app/master/microservices/service/card/CardserviceApplication.java
@@ -1,5 +1,6 @@
 package com.kathesama.app.master.microservices.service.card;
 
+import com.kathesama.app.master.microservices.service.card.util.CardsContactInfo;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,9 +8,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {CardsContactInfo.class})
 /*@ComponentScans({ @ComponentScan("com.eazybytes.cards.controller") })
 @EnableJpaRepositories("com.eazybytes.cards.repository")
 @EntityScan("com.eazybytes.cards.model")*/
