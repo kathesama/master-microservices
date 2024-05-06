a7f26637493d7aa0fc1e85792a05e2e0f06c8ec9
kathemica <kathemica@gmail.com>
2024-05-06 20:29:20 -0300

    added mysql support and migrated from h2

---
+++ build-images.ps1
@@ -0,0 +1,14 @@
+# Lista de todos los microservicios
+$services = "account-service", "loan-service", "card-service", "infrastructure/config-server"
+
+# Itera sobre cada servicio
+foreach ($service in $services)
+{
+    Write-Host "Building Docker image for $service"
+    # Navega al directorio del servicio
+    Set-Location -Path $service
+    # Ejecuta el comando Maven
+    mvn compile jib:dockerBuild
+    # Vuelve al directorio raíz
+    Set-Location -Path ..
+}
