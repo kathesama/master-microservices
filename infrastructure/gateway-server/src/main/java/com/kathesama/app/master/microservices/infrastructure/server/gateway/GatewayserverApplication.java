ff1c19fec94cee183b48764b87960086bf1d621a
kathemica <kathemica@gmail.com>
2024-05-09 15:26:38 -0300

    added gateway server, fixed yupo on infrastructure path

---
+++ infrastructure/gateway-server/src/main/java/com/kathesama/app/master/microservices/infrastructure/server/gateway/GatewayserverApplication.java
@@ -0,0 +1,12 @@
+package com.kathesama.app.master.microservices.infrastructure.server.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+}
